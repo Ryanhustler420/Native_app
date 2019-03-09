f0f026f0823df13d414d27e69000a33b3f237c47
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-10 05:00:52 +0530

    Adding a Person with Services

--- ng-refresher/src/app/persons/person-input.component.ts
+++ ng-refresher/src/app/persons/person-input.component.ts
@@ -1,4 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonService } from './persons.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-person-input',
@@ -6,12 +8,18 @@ import { Component, Output, EventEmitter } from '@angular/core';
     styleUrls: ['./person-input.component.css']
 })
 export class PersonInputComponent {
-    @Output() personCreateEvent = new EventEmitter<string>();
+    // @Output() personCreateEvent = new EventEmitter<string>();
     input = '';
+
+    constructor(private personService: PersonService,
+        // private route: Router
+    ) { }
 
     onCreatePerson() {
         console.log(`created a person ${this.input}`);
-        this.personCreateEvent.emit(this.input);
+        // this.personCreateEvent.emit(this.input);
+        this.personService.addPerson(this.input);
+        // this.route.navigate(['']);
         this.input = '';
     }
 }
