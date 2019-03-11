452a08471994bfbf8681ae622d28b109fd264a01
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-11 22:05:23 +0530

    Building our First own Component

--- ns-core/src/app/app.module.ts
+++ ns-core/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
-import { AppRoutingModule } from "./app-routing.module";
+
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { CurrentChallengeComponent } from './challenges/current-challenge/current-challenge.component';
+
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -17,13 +17,11 @@ import { ItemDetailComponent } from "./item/item-detail.component";
         AppComponent
     ],
     imports: [
-        NativeScriptModule,
-        AppRoutingModule
+    NativeScriptModule
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        CurrentChallengeComponent
     ],
     providers: [],
     schemas: [
