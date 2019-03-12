b1bf8e42a564db75b9002512099128999cc682ef
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-13 04:13:48 +0530

    Added few routes as practice

--- ns-core/src/app/app-routing.module.ts
+++ ns-core/src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { TodayComponent } from './challenges/today/today.component';
+import { CurrentChallengeComponent } from './challenges/current-challenge/current-challenge.component';
+import { ChallengeEditComponent } from './challenges/challenge-edit/challenge-edit.component';
 
 const routes: Routes = [
     { path: '', component: AuthComponent },
-    { path: 'today', component: TodayComponent }
+    { path: 'today', component: TodayComponent },
+    { path: 'current-challenge', component: CurrentChallengeComponent },
+    { path: 'edit-challenge', component: ChallengeEditComponent }
 ];
 
 @NgModule({
--- ns-core/src/app/challenges/current-challenge/current-challenge.component.ts
+++ ns-core/src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { RouterExtensions } from "nativescript-angular/router";
 
 @Component({
     selector: 'ns-current-challenge',
@@ -6,4 +7,11 @@ import { Component } from "@angular/core";
     styleUrls: ['./current-challenge.component.css'],
     moduleId: module.id
 })
-export class CurrentChallengeComponent { }
+export class CurrentChallengeComponent {
+
+    constructor(private router: RouterExtensions) { }
+
+    onEdit() {
+        this.router.navigate(['/edit-challenge']);
+    }
+}
