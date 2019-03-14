27c45a3b239b29743cda8a50b30d5ef0b1713a23
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-14 11:17:26 +0530

    Reading Dynamic Parameters

--- ns-core/src/app/challenges/challenge-edit/challenge-edit.component.ts
+++ ns-core/src/app/challenges/challenge-edit/challenge-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PageRoute } from 'nativescript-angular/router';
 
 @Component({
     selector: 'ns-challenge-edit',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
     styleUrls: ['./challenge-edit.component.css'],
     moduleId: module.id,
 })
-export class ChallengeEditComponent {
+export class ChallengeEditComponent implements OnInit {
+    constructor(private activatedRoute: ActivatedRoute, private pageRoute: PageRoute) { }
 
+    ngOnInit() {
+        // this.activatedRoute.paramMap.subscribe(paramMap => {
+        //     console.log(paramMap.get('mode'));
+        // })
+        // PageRoute can still fetch information even though the page was cache in the stack.
+        // No metter if this page loaded for the first time or retrive from cache
+        // NOTE: Do use this if you know that this page will never be cache
+        this.pageRoute.activatedRoute.subscribe(activatedRoute => {
+            activatedRoute.paramMap.subscribe(paramMap => {
+                console.log(paramMap.get('mode'));
+            });
+        })
+    }
 }
