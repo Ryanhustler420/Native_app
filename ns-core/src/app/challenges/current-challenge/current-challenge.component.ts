d359e8fb16d11fffabb79479f354d3f41a27c7e9
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-15 19:19:01 +0530

    Working on the Current Challenge

--- ns-core/src/app/challenges/current-challenge/current-challenge.component.ts
+++ ns-core/src/app/challenges/current-challenge/current-challenge.component.ts
@@ -7,8 +7,8 @@ import { UIService } from "~/app/shared/ui/ui.service";
     selector: 'ns-current-challenge',
     templateUrl: './current-challenge.component.html',
     styleUrls: [
-        './current-challenge.component.common.css',
-        './current-challenge.component.css'
+        './_current-challenge.component.common.scss',
+        './current-challenge.component.scss'
     ],
     moduleId: module.id
 })
