a29a158c2a6bb6e250d4536ba71a7c61958f541e
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-15 19:39:28 +0530

    Adding the Grid to the Current Challenge

--- ns-core/src/app/challenges/current-challenge/current-challenge.component.ts
+++ ns-core/src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewContainerRef } from "@angular/core";
+import { Component, ViewContainerRef, OnInit } from "@angular/core";
 import { ModalDialogService } from 'nativescript-angular/modal-dialog';
 import { DayModalComponent } from "../day-modal/day-modal.component";
 import { UIService } from "~/app/shared/ui/ui.service";
@@ -12,9 +12,25 @@ import { UIService } from "~/app/shared/ui/ui.service";
     ],
     moduleId: module.id
 })
-export class CurrentChallengeComponent {
+export class CurrentChallengeComponent implements OnInit {
+
+    weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+    days: { dayInMonth: number, dayInWeek: number }[] = [];
 
     constructor(private modalDialog: ModalDialogService, private _vcRef: ViewContainerRef, private uiService: UIService) { }
+
+    ngOnInit() {
+        const currentYear = new Date().getFullYear();
+        const currentMonth = new Date().getMonth();
+        const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+        for (let i = 1; i < daysInMonth + 1; i++) {
+            const date = new Date(currentYear, currentMonth, i);
+            const dayInWeek = date.getDay();
+            this.days.push({ dayInMonth: i, dayInWeek: dayInWeek });
+            console.log(this.days);
+        }
+    }
 
     onChangeStatus() {
         this.modalDialog.showModal(DayModalComponent, {
