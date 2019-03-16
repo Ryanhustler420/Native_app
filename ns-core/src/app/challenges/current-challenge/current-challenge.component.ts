97c4b7ab672cf19b829be320ccd71102104a5254
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-16 08:59:32 +0530

    Populating the Grid

--- ns-core/src/app/challenges/current-challenge/current-challenge.component.ts
+++ ns-core/src/app/challenges/current-challenge/current-challenge.component.ts
@@ -16,19 +16,21 @@ export class CurrentChallengeComponent implements OnInit {
 
     weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
     days: { dayInMonth: number, dayInWeek: number }[] = [];
+    private currentMonth: number;
+    private currentYear: number;
 
     constructor(private modalDialog: ModalDialogService, private _vcRef: ViewContainerRef, private uiService: UIService) { }
 
     ngOnInit() {
-        const currentYear = new Date().getFullYear();
-        const currentMonth = new Date().getMonth();
-        const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+        this.currentYear = new Date().getFullYear();
+        this.currentMonth = new Date().getMonth();
+        const daysInMonth = new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
 
         for (let i = 1; i < daysInMonth + 1; i++) {
-            const date = new Date(currentYear, currentMonth, i);
+            const date = new Date(this.currentYear, this.currentMonth, i);
             const dayInWeek = date.getDay();
             this.days.push({ dayInMonth: i, dayInWeek: dayInWeek });
-            console.log(this.days);
+            // console.log(this.days);
         }
     }
 
@@ -40,5 +42,15 @@ export class CurrentChallengeComponent implements OnInit {
         }).then((status: string) => {
             console.log(status);
         })
+    }
+
+    getRow(index: number, day: { dayInMonth: number, dayInWeek: number }) {
+        const startRow = 1;
+        const weekRow = Math.floor(index / 7);
+        const firstWeekDayOfMonth = new Date(this.currentYear, this.currentMonth, 1).getDay();
+
+        const irregularRow = day.dayInWeek < firstWeekDayOfMonth ? 1 : 0;
+
+        return startRow + weekRow + irregularRow;
     }
 }
