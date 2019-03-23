3d41257c18f84944bc00d7a5936b703ecd8baf3f
Gaurav Gupta <gouravgupta840@gmail.com>
2019-03-23 19:06:14 +0530

    Submitting Form Inputs

--- ns-core/src/app/challenges/challenge-edit/challenge-edit.component.ts
+++ ns-core/src/app/challenges/challenge-edit/challenge-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PageRoute } from 'nativescript-angular/router';
+import { PageRoute, RouterExtensions } from 'nativescript-angular/router';
 
 @Component({
     selector: 'ns-challenge-edit',
@@ -10,7 +10,7 @@ import { PageRoute } from 'nativescript-angular/router';
 })
 export class ChallengeEditComponent implements OnInit {
     isCreating = true;
-    constructor(private activatedRoute: ActivatedRoute, private pageRoute: PageRoute) { }
+    constructor(private activatedRoute: ActivatedRoute, private pageRoute: PageRoute, private router: RouterExtensions) { }
 
     ngOnInit() {
         // this.activatedRoute.paramMap.subscribe(paramMap => {
@@ -29,5 +29,10 @@ export class ChallengeEditComponent implements OnInit {
                 }
             });
         })
+    }
+
+    onSubmit(title: string, description: string) {
+        console.log(title, description);
+        this.router.backToPreviousPage();
     }
 }
